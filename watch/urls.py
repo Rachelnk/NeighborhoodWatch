5f1be9f3038d6fc09bc6fc1bf3098d58fc02080c
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-18 12:18:42 +0300

    Include content app urls modules in project's urls module.

--- watch/urls.py
+++ watch/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('watch.urls'))
+    path('',include('content.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
