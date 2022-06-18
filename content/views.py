11f7ce4ea34198b4ecfa4110c0ec9274c383e9ff
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-18 12:40:17 +0300

    Import models, shortcuts in views module.

--- content/views.py
+++ content/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Profile, Post, Neighborhood, Business
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
