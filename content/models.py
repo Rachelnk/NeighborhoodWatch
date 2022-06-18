90211ac7d908897c4b697c9401c6ab4efe0ae0a2
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-18 10:39:02 +0300

    Create neighborhood class model and it's fields.

--- content/models.py
+++ content/models.py
@@ -1,9 +1,99 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary import CloudinaryImage
+from cloudinary.models import CloudinaryField
+
+counties = [
+    ('', ('Choose')), 
+    ('Baringo', ('Baringo')),
+    ('Bomet', ('Bomet')),
+    ('Bungoma ', ('Bungoma ')),
+    ('Busia', ('Busia')),
+    ('Elgeyo Marakwet', ('Elgeyo Marakwet')),
+    ('Embu', ('Embu')),
+    ('Garissa', ('Garissa')),
+    ('Homa Bay', ('Homa Bay')),
+    ('Isiolo', ('Isiolo')),
+    ('Kajiado', ('Kajiado')),
+    ('Kakamega', ('Kakamega')),
+    ('Kericho', ('Kericho')),
+    ('Kiambu', ('Kiambu')),
+    ('Kilifi', ('Kilifi')),
+    ('Kirinyaga', ('Kirinyaga')),
+    ('Kisii', ('Kisii')),
+    ('Kisumu', ('Kisumu')),
+    ('Kitui', ('Kitui')),
+    ('Kwale', ('Kwale')),
+    ('Laikipia', ('Laikipia')),
+    ('Lamu', ('Lamu')),
+    ('Machakos', ('Machakos')),
+    ('Makueni', ('Makueni')),
+    ('Mandera', ('Mandera')),
+    ('Meru', ('Meru')),
+    ('Migori', ('Migori')),
+    ('Marsabit', ('Marsabit')),
+    ('Mombasa', ('Mombasa')),
+    ('Muranga', ('Muranga')),
+    ('Nairobi', ('Nairobi')),
+    ('Nakuru', ('Nakuru')),
+    ('Nandi', ('Nandi')),
+    ('Narok', ('Narok')),
+    ('Nyamira', ('Nyamira')),
+    ('Nyandarua', ('Nyandarua')),
+    ('Nyeri', ('Nyeri')),
+    ('Samburu', ('Samburu')),
+    ('Siaya', ('Siaya')),
+    ('Taita Taveta', ('Taita Taveta')),
+    ('Tana River', ('Tana River')),
+    ('Tharaka Nithi', ('Tharaka Nithi')),
+    ('Trans Nzoia', ('Trans Nzoia')),
+    ('Turkana', ('Turkana')),
+    ('Uasin Gishu', ('Uasin Gishu')),
+    ('Vihiga', ('Vihiga')),
+    ('Wajir', ('Wajir')),
+    ('West Pokot', ('West Pokot')),
+]
+
+business_type = [('cleaning',('cleaning')),
+('grocery',('grocery')),
+('bakery',('bakery')),
+('salon',('salon')),
+('barbershop',('barbershop')),
+('butchery',('butchery')),
+('chemist',('chemist')),
+('general_shop',('general_shop')),
+('milk_atm',('milk_atm')),
+]
+post_type = [
+    ('1', 'Crimes and Safety'),
+    ('2', 'Health Emergency'),
+    ('3', 'Recommendations e.g Plumber, mamafua'),
+    ('4', 'Power Outages'),
+    ('5', 'Lost and Found'),
+    ('6', 'Death'),
+    ('7', 'Event'),
+    ('8', ('water'))
+]
 
 # Create your models here.
+
+class neighborhood(models.Model):
+    name = models.TextField(max_length=20, verbose_name='Hood Title', null=True)
+    description = models.TextField(max_length=254, blank=True, verbose_name='Description')
+    location = models.CharField(max_length=150, verbose_name='Location', null=True, blank=True)
+    county = models.CharField(choices=counties, max_length=150, verbose_name='County', null=True, blank=True)
+    photo = CloudinaryField('image')
+    admin =  models.ForeignKey('User', on_delete=models.CASCADE, verbose_name= 'Admin', null=True, blank=True)
+
+
+
 class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', null=True)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField()
+    email = models.EmailField(max_length=100, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created', null= True)
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Date Updated', null= True)
 
 class Post(models.Model):
     title = models.CharField(max_length=500, verbose_name='Title', null=True)
