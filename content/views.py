5daabe2faea15a561c5ec612fb7c94f404e50aff
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-19 12:23:49 +0300

    Create view function to handle add post form requests.

--- content/views.py
+++ content/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
+import profile
+from urllib.response import addbase
 from django.shortcuts import render, redirect
-from .models import Profile, Post, Neighborhood, Business
+from .models import Membership, Profile, Post, Neighborhood, Business
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
@@ -68,7 +70,9 @@ def signup(request):
   return render(request,'signup.html')
 
 @login_required(login_url='login')
-def addneighborhood(request):
+def add_neighborhood(request, username):
+  profile = User.objects.get(username=username)
+  profile_details = Profile.objects.get(user=profile.id)  
   form = AddNeighborhoodForm()
   if request.method == 'POST':
     form = AddNeighborhoodForm(request.POST, request.FILES)
@@ -78,23 +82,92 @@ def addneighborhood(request):
       hood.profile = request.user.profile
       hood.save()
       messages.success(request, "Neighborhood Added Successfully")
-      return redirect('index')
+      return redirect('my_neighborhoods', username = username)
     else:
       messages.error(request, "Neighborhood wasn't created.")
-      return redirect ('addneighborhood')
+      return redirect ('add_neighborhood')
 
   else:
     form = AddNeighborhoodForm()
 
-  return render(request, addneighborhood.html, {'form': form})
+  return render(request, add_neighborhood.html, {'form': form, 'profile_details':profile_details})
 
 @login_required(login_url='login')
-def create_hood(request):
-  return render(request)
+def my_neighborhoods(request, username):
+  profile = User.objects.get(username=username)
+  profile_details =  Profile.objects.get(user = profile.id)
+  hoods = Neighborhood.objects.filter(admin = profile.id)
+  return render (request, 'my_neighborhoods.html', {'profile_details':profile_details})
 
 @login_required(login_url='login')
-def create_hood(request):
-  return render(request)
+def add_business(request, username):
+  profile = User.objects.get(username = username)
+  profile_details = Profile.objects.get(user = profile.id)
+  form = AddBusinessForm()
+  if request.method == 'POST':
+    form = AddBusinessForm(request.POST)
+    if form.is_valid():
+      title = form.cleaned_data['title']
+      email = form.cleaned_data['email']
+      neighborhood = form.cleaned_data['neighborhood']
+      description = form.cleaned_data['description']
+      hood = Neighborhood.objects.get(pk=int(neighborhood))
+      member = Membership.objects.filter(user = profile.id, neighborhood_membership = hood )
+
+      if not member:
+          messages.error(request,"Not a member of this neighborhood thus you cannot add a business. Join the neighborhood first.")
+          return redirect('my_businesss', username=username)
+
+      else:
+        hood = Neighborhood.objects.get(pk=int(neighborhood))
+        new_business = Business(title = title, email = email, neighbourhood = hood, description = description, owner = request.user.profile )
+        business = form.save(commit=False)
+        new_business.save()
+        
+        messages.success(request,"Business was created successfully.")
+        return redirect ('my_businesses', username = username)
+    else:
+      messages.error(request, "Business wasn't created.")
+      return redirect ('add_business')
+  else:
+    form = AddBusinessForm()
+    
+  return render(request, 'add_business.html', {'form':form})
+
+@login_required(login_url='login')
+def my_businesses(request):
+
+@login_required(login_url='login')
+def add_post(request, username):
+  profile = User.objects.get(username = username)
+  profile_details = Profile.objects.get(user= profile.id)  
+  if request.method == 'POST':
+    form = AddPostForm(request.POST)
+    if form.is_valid():
+      title=form.cleaned_data['title']
+      description=form.cleaned_data['description']
+      category=form.cleaned_data['category']
+      neighborhood=form.cleaned_data['neighborhood']
+      
+      hood = Neighborhood.objects.get(pk=int(neighborhood))
+      member = Membership.objects.filter(user = profile.id, neighbourhood_membership = hood)
+      if not member:
+        messages.success(request,'You have to be a member of this neighborhood to add a post.')
+        return redirect('add_post', username = username)
+      else:
+        hood = Neighborhood.objects.get(pk=int(neighborhood))
+        new_post = Post(title = title, description = description, category= category, neighborhood = neighborhood)
+        new_post.save()
+
+        messages.success(request, 'Your post was successfully created.')
+        return redirect('my_posts', username = username )
+    else:
+      messages.error(request, "Your Post Wasn't Created")
+      return redirect(add_post, username = username)
+  else:
+    form = AddPostForm()
+
+  return render(request, 'add_post.html', {'form':form})
 
 
 
